b8ede80e3135ffc58a9339eb49f5639b8ed8e26d
sanket-bhiwapurkar <sanket24bhiwapurkar@gmail.com>
2023-02-10 12:21:15 +0000

    todo app part 1

---
+++ index.js
@@ -0,0 +1,14 @@
+const element = (
+  <div className="bg-container">
+    <h1 className="heading">Super Over League</h1>
+    <div>
+      <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" />
+      <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
+//document.getElementById("root").classList.add("bg-container");
+//<img src='https://assets.ccbp.in/frontend/react-js/rcb-img.png', className="image"/>
+//<img src='https://assets.ccbp.in/frontend/react-js/csk-img.png', className="image"/>
